c659c4d427958661283829e4aeac8bdb54ad0797
Raymond Lee <raymond102033@gmail.com>
2021-07-13 00:44:26 +0800

    feat: test env

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,10 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { PrismaClient } from "@prisma/client";
-
-const client = new PrismaClient();
 
 export default async (req, res) => {
   res
     .status(200)
-    .json({ name: "John Doe", user: await client.user.findMany({}) });
+    .json({ name: "John Doe", test: process.env.TEST });
 };
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const APP_URL = process.env.APP_URL;
+  const NEXT_PUBLIC_APP_URL = process.env.NEXT_PUBLIC_APP_URL;
   return (
     <div className={styles.container}>
       <Head>
@@ -16,8 +18,14 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
+        <h2 className={styles.title}>APP_URL: {APP_URL}</h2>
+
+        <h2 className={styles.title}>
+          NEXT_PUBLIC_APP_URL: {NEXT_PUBLIC_APP_URL}
+        </h2>
+
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -58,12 +66,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
