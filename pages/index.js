d6b276a199f8f5dffba7331d800bea8455cafc37
Raymond Lee <raymond102033@gmail.com>
2021-07-22 00:31:51 +0800

    feat: modify run container command

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Excellent!!!
+          Excellent!!!!!
         </h1>
 
         <h1 className={styles.title}>
